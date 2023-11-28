41e4ecccf997fab024479e0c4aaa9e8c16300fdf
“Quaya <dbrown1@svsu.edu>
2023-11-28 03:58:08 -0500

    first commit

---
+++ Scripts/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["","Software Engineer", "Full Stack Web Developer", "UX/UI Designer", "App Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
